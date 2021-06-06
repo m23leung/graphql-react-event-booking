6a2ffcd6ad0b6f038d3a4390b4cf5a7513bd376c
Mark Leung <mark.leung89@gmail.com>
2021-06-06 20:01:30 +1000

    Complete draft of backend section

--- app.js
+++ app.js
@@ -1,162 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
+const isAuth = require("./middleware/is-auth");
 
-const Event = require("./models/event");
-const User = require("./models/user");
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use(isAuth);
+
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        type User {
-            _id: ID!
-            email: String!
-            password: String
-        }
-
-        input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input UserInput {
-            email: String!
-            password: String!
-        }
-
-        type RootQuery {
-            events: [Event!]!
-        }
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-            createUser(userInput: UserInput): User
-        }
-
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-    rootValue: {
-      events: () => {
-        return Event.find()
-          .then((events) => {
-            return events.map((event) => {
-              return { ...event._doc, _id: event.id };
-            });
-          })
-          .catch((err) => {
-            throw err;
-          });
-      },
-      createEvent: async (args) => {
-        try {
-          const { title, description, price, date } = args.eventInput;
-          const event = new Event({
-            title,
-            description,
-            price: +price,
-            date: new Date(date),
-            creator: "60aa1555d3ef20ee76a91eb1",
-          });
-          const savedEvent = await event.save();
-          const user = await User.findById("60aa1555d3ef20ee76a91eb1");
-
-          // If user is not found, error will be thrown
-          await user.createdEvents.push(event);
-          await user.save();
-          return savedEvent;
-        } catch (err) {
-          throw new Error("event not created", err);
-        }
-      },
-      createEventOld: (args) => {
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date),
-          creator: "60aa1555d3ef20ee76a91eb1",
-        });
-        return event
-          .save()
-          .then((result) => {
-            createdEvent = { ...result._doc, _id: result._doc._id.toString() };
-            return User.findById("60aa1555d3ef20ee76a91eb1");
-            let createdEvent;
-          })
-          .then((user) => {
-            if (!user) {
-              throw new Error("User not found.");
-            }
-            user.createdEvents.push(event);
-            return user.save();
-          })
-          .then((result) => {
-            return createdEvent;
-          })
-          .catch((err) => {
-            console.log(err);
-            throw err;
-          });
-      },
-      createUser: async (args) => {
-        try {
-          const { email, password } = args.userInput;
-          const existingUser = await User.findOne({ email });
-          if (existingUser) {
-            throw new Error("user already exists");
-          }
-          const user = new User({
-            email,
-            password: bcrypt.hashSync(password, 10),
-          });
-          const res = await user.save();
-          return { ...res._doc, password: null };
-        } catch (err) {
-          throw new Error("user not created", err);
-        }
-      },
-      createUserOld: (args) => {
-        return User.findOne({ email: args.userInput.email })
-          .then((user) => {
-            if (user) {
-              throw new Error("User exists already.");
-            }
-            return bcrypt.hash(args.userInput.password, 12);
-          })
-          .then((hashedPassword) => {
-            const user = new User({
-              email: args.userInput.email,
-              password: hashedPassword,
-            });
-            return user.save();
-          })
-          .then((result) => {
-            return { ...result._doc, password: null, _id: result.id };
-          })
-          .catch((err) => {
-            throw err;
-          });
-      },
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true,
   })
 );
